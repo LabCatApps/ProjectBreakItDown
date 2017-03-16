0f6db3abd4bdef22019ef36e2982579af2edfb20
Kate <labcatapps@gmail.com>
2017-03-16 20:14:41 +0000

    fixed expandable list adapter

--- app/src/main/java/com/example/kate/projectbreakitdown/TaskListAdapter.java
+++ app/src/main/java/com/example/kate/projectbreakitdown/TaskListAdapter.java
@@ -1,62 +1,90 @@
 package com.example.kate.projectbreakitdown;
 
 import android.content.Context;
-import android.database.DataSetObserver;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ExpandableListAdapter;
+import android.widget.BaseExpandableListAdapter;
 import android.widget.TextView;
 
+
 import java.util.ArrayList;
-import java.util.HashMap;
 
 /**
  * Created by Kate on 12/03/2017.
  */
 
-public class TaskListAdapter implements ExpandableListAdapter {
+public class TaskListAdapter extends BaseExpandableListAdapter {
 
     private Context context;
-    private ArrayList<Task> task_parent;
-    private HashMap<String,Action> task_children;
+    private final ArrayList<Task> task_list;
+    private final LayoutInflater inflater;
 
-    public TaskListAdapter(Context context, ArrayList<Task> task_parent, HashMap<String, Action> task_children){
+    public TaskListAdapter(Context context, ArrayList<Task> task_list){
         this.context = context;
-        this.task_parent = task_parent;
-        this.task_children = task_children;
+        this.task_list = task_list;
+        this.inflater = LayoutInflater.from(context);
     }
 
     @Override
-    public void registerDataSetObserver(DataSetObserver observer) {
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        Task task = (Task) getGroup(groupPosition);
+        if (convertView == null){
+            convertView = inflater.inflate(R.layout.task_view,null);
+        }
 
+        TextView title = (TextView) convertView.findViewById(R.id.task_title);
+        title.setText(task.getTask_title().trim());
+        TextView description = (TextView) convertView.findViewById(R.id.task_description);
+        description.setText(task.getTask_description().trim());
+        TextView duedate = (TextView) convertView.findViewById(R.id.task_duedate);
+        duedate.setText("Due: " + task.getTask_duedate().trim());
+        TextView priority = (TextView) convertView.findViewById(R.id.task_priority);
+        priority.setText("Priority: " + task.getTask_priority().trim());
+        TextView length = (TextView) convertView.findViewById(R.id.task_length);
+        length.setText("Length (mins): " + task.getTask_length().trim());
+
+        return convertView;
     }
 
     @Override
-    public void unregisterDataSetObserver(DataSetObserver observer) {
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        Action action = (Action) getChild(groupPosition,childPosition);
+        if (convertView == null){
+            convertView = inflater.inflate(R.layout.action_view, null);
+        }
+        TextView title = (TextView) convertView.findViewById(R.id.action_title);
+        title.setText(action.getName().trim());
+        TextView description = (TextView) convertView.findViewById(R.id.action_description);
+        description.setText(action.getDescription().trim());
+        TextView priority = (TextView) convertView.findViewById(R.id.action_priority);
+        priority.setText("Priority: " + action.getPriority().trim());
+        TextView length = (TextView) convertView.findViewById(R.id.action_length);
+        length.setText("Length: " + action.getLength().trim());
+
+        return convertView;
 
     }
 
     @Override
     public int getGroupCount() {
-        return task_parent.size();
+        return task_list.size();
     }
 
     @Override
     public int getChildrenCount(int groupPosition) {
-        ArrayList<Action> task_children = task_parent.get(groupPosition).getActions();
-        return task_children.size();
+        return task_list.get(groupPosition).getActions().size();
     }
 
     @Override
-    public Object getGroup(int groupPosition) {
-        return task_parent.get(groupPosition);
+    public Task getGroup(int groupPosition) {
+        return task_list.get(groupPosition);
     }
 
     @Override
-    public Object getChild(int groupPosition, int childPosition) {
-        //ArrayList<Action> task_children = task_parent.get(groupPosition).getActions();
-        return task_children.get(task_parent.get(groupPosition).getTask_title());
+    public Action getChild(int groupPosition, int childPosition) {
+        return task_list.get(groupPosition).getActions().get(childPosition);
+
     }
 
     @Override
@@ -75,78 +103,7 @@ public class TaskListAdapter implements ExpandableListAdapter {
     }
 
     @Override
-    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
-        Task taskInfo = (Task) getGroup(groupPosition);
-        if (convertView == null){
-            LayoutInflater inf = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inf.inflate(R.layout.task_view, null);
-        }
-
-        TextView task_title = (TextView) convertView.findViewById(R.id.task_title);
-        task_title.setText(taskInfo.getTask_title().trim());
-        TextView task_duedate = (TextView) convertView.findViewById(R.id.task_duedate);
-        task_duedate.setText("Due: " + taskInfo.getTask_duedate().toString());
-        TextView task_priority = (TextView) convertView.findViewById(R.id.task_priority);
-        task_priority.setText("Priority: " + taskInfo.getTask_priority());
-        TextView task_length = (TextView) convertView.findViewById(R.id.task_length);
-        task_length.setText("Length (min): " + taskInfo.getTask_length());
-        TextView task_description = (TextView) convertView.findViewById(R.id.task_description);
-        task_description.setText(taskInfo.getTask_description());
-        //NEED TO ADD IMAGE SETTER
-        return convertView;
-    }
-
-    @Override
-    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-        Action actionInfo = (Action) getChild(groupPosition, childPosition);
-        if (convertView == null){
-            LayoutInflater childinf = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = childinf.inflate(R.layout.action_view, null);
-        }
-        TextView action_name = (TextView) convertView.findViewById(R.id.action_title);
-        action_name.setText(actionInfo.getName().trim());
-        TextView action_priority = (TextView) convertView.findViewById(R.id.action_priority);
-        action_priority.setText("Priority: " + actionInfo.getPriority());
-        TextView action_length = (TextView) convertView.findViewById(R.id.action_length);
-        action_length.setText("Length (min): " + actionInfo.getLength());
-        TextView action_description = (TextView) convertView.findViewById(R.id.action_description);
-        action_description.setText(actionInfo.getDescription());
-
-        return convertView;
-    }
-
-    @Override
     public boolean isChildSelectable(int groupPosition, int childPosition) {
         return true;
-    }
-
-    @Override
-    public boolean areAllItemsEnabled() {
-        return false;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
-
-    @Override
-    public void onGroupExpanded(int groupPosition) {
-
-    }
-
-    @Override
-    public void onGroupCollapsed(int groupPosition) {
-
-    }
-
-    @Override
-    public long getCombinedChildId(long groupId, long childId) {
-        return 0;
-    }
-
-    @Override
-    public long getCombinedGroupId(long groupId) {
-        return 0;
     }
 }
